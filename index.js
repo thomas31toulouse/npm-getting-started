5215874c58a4566c4e06c7003c118b7dbcbe94ac
thomas31toulouse <thomas-jondeau@hotmail.fr>
2021-03-21 15:09:21 +0100

    cowsay

---
+++ index.js
@@ -0,0 +1,12 @@
+const myInformation = require ('./information.js');
+myInformation();
+
+const myInfo = require('./information.js');
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : message,
+    e : "oO",
+    T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,9 @@
+const myInfo = {
+    name:"Thomas",
+    campus: "Toulouse"
+}
+
+const myInfos= () => {
+     message = `hello !! my name is ${myInfo.name}, from ${myInfo.campus}`;
+};
+module.exports = myInfos;
